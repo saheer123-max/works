b4852ef6e1ea8f870b9c6306c97af4f317937be4
saheer123-max <saheeraslam39@gmail.com>
2025-02-16 22:05:29 +0530

    update

--- 23.js
+++ 23.js
@@ -1,3 +1,71 @@
-var sortBy = function(arr, fn) {
-    return arr.sort((a,b)=>fn(a)-fn(b))
-};
+// var sortBy = function(arr, fn) {
+//     return arr.sort((a,b)=>fn(a)-fn(b))
+// };
+
+
+//////////
+
+// let text="hallo world"
+
+// let result=text.split("").reverse().join("")
+// console.log(result);
+
+
+//no:9////////////palindrom//////////////////////
+
+ 
+// let palindrom=function(x){
+
+//   let str=x.toString();
+//    let resu=str.split("").reverse().join("")
+//     return str===resu
+// }
+// console.log(palindrom(120))
+
+
+
+
+
+///Array Wrapper/////////////
+
+
+
+
+
+
+function arrayWrapper(nums) {
+    return {
+      valueOf: function () {
+        return nums.reduce((sum, num) => sum + num, 0);
+      },
+      toString: function () {
+        return `[${nums.join(",")}]`;
+      },
+    };
+  }
+  
+  // Example Usage
+      const obj1 = arrayWrapper([1, 2, 3]);
+      const obj2 = arrayWrapper([4, 5]);
+  
+      console.log(obj1 + obj2);   // Output: 15
+      console.log(String(obj1));  // Output: [1,2,3]
+      console.log(String(obj2));  // Output: [4,5]
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 26.js
+++ 26.js
@@ -1,6 +1,15 @@
-var isPalindrome = function(x) {
-    let str = x.toString();
-   let rev =str.split('').reverse().join('');
-   return str === rev;
+// var isPalindrome = function(x) {
+//     let str = x.toString();
+//    let rev =str.split('').reverse().join('');
+//    return str === rev;
 
-};
+
+
+
+// };
+
+
+let words = ["Hello", "World"];
+console.log(words.length);      // Output: 2
+console.log(words.length - 1);  // Output: 1
+console.log(words[1]);          // Output: "World"
