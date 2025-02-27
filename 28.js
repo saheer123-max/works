28c63d0c2b58c1b37bee4a5c34b794830060f0c6
saheer123-max <saheeraslam39@gmail.com>
2025-02-27 13:44:05 +0530

    update

--- 28.js
+++ 28.js
@@ -20,28 +20,16 @@
 //   return a === b ? -1 : Math.max(a.length, b.length);
 // }
  
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import TextContext from "./Textcontext"; 
 
-function Text() {
-  const navigate = useNavigate();
-  const { setText } = useContext(TextContext); 
-  const {setdata}=useContext(TextContext)
-  return (
-    <div>
-      <input 
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <textarea rows={4} placeholder="Type something"
-      
-      onChange={(e)=>setdata(e.target.value)}
-      ></textarea>
-      <button onClick={() => navigate("/")}>Submit</button>
-    </div>
-  );
-}
 
-export default Text;
 
+
+var searchInsert = function(nums, target) {
+  let i = 0, j = nums.length;
+  while (i < j) {
+      let mid = Math.floor((i + j) / 2);
+      if (nums[mid] < target) i = mid + 1;
+      else j = mid;
+  }
+  return i;
+};
