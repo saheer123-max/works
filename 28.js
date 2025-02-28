7e9b8f7cb101ee412f597af37fee87e4dcda8742
saheer123-max <saheeraslam39@gmail.com>
2025-02-28 20:36:26 +0530

    update

--- 28.js
+++ 28.js
@@ -24,12 +24,47 @@
 
 
 
-var searchInsert = function(nums, target) {
-  let i = 0, j = nums.length;
-  while (i < j) {
-      let mid = Math.floor((i + j) / 2);
-      if (nums[mid] < target) i = mid + 1;
-      else j = mid;
-  }
-  return i;
-};
+// var searchInsert = function(nums, target) {
+//   let i = 0, j = nums.length;
+//   while (i < j) {
+//       let mid = Math.floor((i + j) / 2);
+//       if (nums[mid] < target) i = mid + 1;
+//       else j = mid;
+//   }
+//   return i;
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        int jan, feb, mar, avg, total;
+        Console.Write(" enter jan attendence");
+        jan=int.Parse(Console.ReadLine());
+        Console.WriteLine("enter feb attendence");
+        feb = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter mar attendence");
+        mar = int.Parse(Console.ReadLine());
+        total = jan + feb + mar;
+        Console.WriteLine($"tottal is{ total }");
+        avg = total / 3;
+        Console.WriteLine($" avg iss{avg}");
+
+
+    }
+}
